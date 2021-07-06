8fb22e7043dd8a7d95a7e7cf61038ceb2f7acc43
Odebisi Solomon idowu <Odebisiidowu09@gmail.com>
2021-07-06 20:28:42 +0100

    uploading files

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const lower = document.getElementById("lower");
+const add = document.getElementById("add");
+let no = document.getElementById("number");
+
+lower.addEventListener("click", decrement);
+add.addEventListener("click", increment);
+
+let number = 50;
+
+function increment() {
+  if (number === 50) return;
+  console.log("Hello");
+  console.log(number);
+  number += 1;
+}
+
+function decrement() {
+  if (number <= 0) return;
+  number -= 1;
+}
+
+setInterval(() => {
+  no.innerText = number;
+
+  let colo = "";
+  switch (number) {
+    case 0:
+      colo = "red";
+      break;
+    case 50:
+      colo = "purple";
+      break;
+    default:
+      colo = "green";
+      break;
+  }
+  no.style.color = colo;
+}, 100);
